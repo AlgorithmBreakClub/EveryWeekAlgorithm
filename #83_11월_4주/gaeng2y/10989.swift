78c170af256516a3cbd2081bbeffe2f79c786c47
gaeng2y <gaeng2y@gmail.com>
2022-11-27 17:44:47 +0900

    11월 4주

---
+++ #83_11월_4주/gaeng2y/10989.swift
@@ -0,0 +1,65 @@
+import Foundation
+
+// 라이노님의 FileIO 클래스
+final class FileIO {
+    private var buffer:[UInt8]
+    private var index: Int
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        buffer = Array(fileHandle.readDataToEndOfFile())+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+        index = 0
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer.withUnsafeBufferPointer { $0[index] }
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+            || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45{ isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var str = ""
+        var now = read()
+
+        while now == 10
+            || now == 32 { now = read() } // 공백과 줄바꿈 무시
+
+        while now != 10
+            && now != 32 && now != 0 {
+                str += String(bytes: [now], encoding: .ascii)!
+                now = read()
+        }
+
+        return str
+    }
+}
+
+let file = FileIO()
+
+let N = file.readInt()
+var dp = Array(repeating:0,count:10001)
+for _ in 0..<N {
+    let q = file.readInt()
+    dp[q]+=1
+}
+var answer = ""
+for i in 1...10000 {
+    var j = 0
+    answer += String(repeating:"\(i)\n",count:dp[i])
+}
+print(answer)
